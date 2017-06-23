9c4660122fc5191d4e3ce4139730c8fb5cd75eb8
eliza0x <me@eliza.link>
2017-06-23 13:01:27 +0900

    support: 5 6

--- emoji.py
+++ emoji.py
@@ -1,26 +1,9 @@
 import os
 import collections
+from tqdm import tqdm
 
 from char_set import alphabet, katakana, hiragana,  mark
-from char_set import kanji1, kanji2, kanji3, kanji4
-
-"""
-auther:
-    Sohei Yamaga <me@eliza.link>
-licence: 
-    MIT LICENCE
-dependents:
-    ImageMagick, Python3
-shell: 
-    convert -pointsize 140
-            -font /usr/share/fonts/TTF/Mplus1p-Black.ttf 
-            -annotate 0 $1
-            -gravity center
-            -fill black
-            -size 128x128
-            xc:none
-            $1.png
-"""
+from char_set import kanji1, kanji2, kanji3, kanji4, kanji5, kanji6
 
 def make_emoji(path: str, file_name: str, text: str):
     os.system(
@@ -35,18 +18,15 @@ def make_emoji(path: str, file_name: str, text: str):
       )
 
 def create_img(path ,pre ,data):
-    print("creating emoji: "+path+" case", end="")
     os.system("mkdir " + path)
     already = collections.defaultdict(lambda: 0)
     cnt = 0
-    for char_data in data:
+    for char_data in tqdm(data):
         key = char_data[0]
         value = char_data[1]
         if(0<already[value]):
             value= value + str(already[value])
         make_emoji(path, pre+value, key)
-        print('.', end='')
-    print("")
 
 os.system("mkdir " + "Out")
 create_img("Out/Lower"    , "l"        , alphabet.alpha    )
@@ -54,8 +34,10 @@ create_img("Out/Upper"    , "u"        , alphabet.ualpha   )
 create_img("Out/Hiragana" , "j"        , hiragana.hiragana )
 create_img("Out/Katakana" , "k"        , katakana.katakana )
 create_img("Out/Mark"     , "m"        , mark.mark     )
-create_img("Out/Kanji1rd" , "kanji_"   , kanji1.kanji1   )
-create_img("Out/Kanji2rd" , "kanji_2_" , kanji2.kanji2   )
-create_img("Out/Kanji3rd" , "kanji_3_" , kanji3.kanji3   )
-create_img("Out/Kanji4rd" , "kanji_4_" , kanji4.kanji4   )
+create_img("Out/Kanji1" , "kanji_"   , kanji1.kanji1   )
+create_img("Out/Kanji2" , "kanji_2_" , kanji2.kanji2   )
+create_img("Out/Kanji3" , "kanji_3_" , kanji3.kanji3   )
+create_img("Out/Kanji4" , "kanji_4_" , kanji4.kanji4   )
+create_img("Out/Kanji5" , "kanji_5_" , kanji5.kanji5   )
+create_img("Out/Kanji6" , "kanji_6_" , kanji6.kanji6   )
 
